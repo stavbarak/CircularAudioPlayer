6337e09b48701e482e4e43460372f1e4de063f19
Tom Klinovsky <tom.klino@gmail.com>
2014-05-05 13:12:56 -0400

    added a rotation calculation as preperation for animation

--- js/player-ui.js
+++ js/player-ui.js
@@ -1,5 +1,26 @@
 function buildPlayerUI(){
-    var paper = Raphael(10, 50, 320, 200);
-    var circle = paper.circle(160,100,50);
-    var pizzaSlice = paper.path("M160,100 l0,-60 a50,50 0 0,1 60,60 z")
+    //rotation constants a = sin(pi/12) b = cos(pi/12)
+    var a = 0.259, b = 0.966;
+    
+    var ccX = 160, ccY = 100, r = 60;
+    
+    var firstpX = ccX - r;
+    var firstpY = ccY;
+    var firstp = firstpX + "," + firstpY;
+    var paper = Raphael(0, 0, 320, 200);
+    var circle = paper.circle(ccX,ccY,r);
+    var x = ((firstpX - ccX) * b) - ((firstpY -ccY) * a) + ccX,
+        y = ((firstpX -ccX) * a) + ((firstpY-ccY) * a) +ccY;
+    var goto = x + "," + y;
+    var pizzaSlice = paper.path("M160,100 L" + firstp + " A60,60 0 0,1 " + goto + " z");
+    pizzaSlice.attr("fill", "#43E4E4");
+    
+    firstpX = x;
+    firstpY = y;
+    firstp = firstpX + "," + firstpY;
+    x = ((firstpX - ccX) * b) - ((firstpY -ccY) * a) + ccX;
+    y = ((firstpX -ccX) * a) + ((firstpY-ccY) * a) +ccY;
+    goto = x + "," + y;
+    pizzaSlice2 = paper.path("M160,100 L" + firstp + " A60,60 0 0,1 " + goto + " z");
+    pizzaSlice2.attr("fill", "#FFCC22");
 }
