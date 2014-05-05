22c7fa9dbe2f189107915a3e61476140cd10af70
Tom Klinovsky <tom.klino@gmail.com>
2014-05-05 10:20:41 -0400

    added a sample of the pizza slice that will represent a song

--- js/player-ui.js
+++ js/player-ui.js
@@ -1,4 +1,5 @@
 function buildPlayerUI(){
     var paper = Raphael(10, 50, 320, 200);
-    var circle = paper.circle(60,50,50);
+    var circle = paper.circle(160,100,50);
+    var pizzaSlice = paper.path("M160,100 l0,-60 a50,50 0 0,1 60,60 z")
 }
