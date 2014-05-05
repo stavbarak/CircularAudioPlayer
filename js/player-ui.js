09ceed6d6954aa107c717f24e55506cf5c66b4ac
Tom Klinovsky <tom.klino@gmail.com>
2014-05-05 09:07:47 -0400

    added general shape

--- js/player-ui.js
+++ js/player-ui.js
@@ -1,4 +1,4 @@
 function buildPlayerUI(){
-    //var paper = Raphael(10, 50, 320, 200);
-    //var circle = paper.circle(40,50,10);
+    var paper = Raphael(10, 50, 320, 200);
+    var circle = paper.circle(60,50,50);
 }
